e7829abd7b4211533284315344cad4e2e9326308
Menderes <mndrs.kc@gmail.com>
2022-04-24 23:59:21 -0400

    deploying

--- server.js
+++ server.js
@@ -16,10 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useUnifiedTopology: true,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/bufgettracker', {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
 
 // routes
